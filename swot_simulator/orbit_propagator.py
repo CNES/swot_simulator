37581dae9f3ebd98b512a1a7496dc50f937bc7c6
Lucile Gaultier <lucile.gaultier@oceandatalab.com>
2020-03-19 07:29:43 +0100

    Rename math library to avoid conflict, add alcycle variable, necessary to compute errors

--- swot_simulator/orbit_propagator.py
+++ swot_simulator/orbit_propagator.py
@@ -9,7 +9,7 @@ Orbit Propagator
 from typing import Dict, Iterable, Optional, TextIO, Tuple
 import logging
 import numpy as np
-from . import math
+from . import math_func
 from . import settings
 from . import VOLUMETRIC_MEAN_RADIUS
 
@@ -46,14 +46,14 @@ def load_ephemeris(
 def interpolate(lon: np.ndarray, lat: np.ndarray,
                 time: np.ndarray) -> np.ndarray:
     """Interpolate the given orbit at high resolution (0.5 seconds)"""
-    x, y, z = math.spher2cart(lon, lat)
+    x, y, z = math_func.spher2cart(lon, lat)
     time_hr = np.arange(time[0], time[-1], 0.5, dtype=time.dtype)
 
     x = np.interp(time_hr, time, x)
     y = np.interp(time_hr, time, y)
     z = np.interp(time_hr, time, z)
 
-    lon, lat = math.cart2spher(x, y, z)
+    lon, lat = math_func.cart2spher(x, y, z)
 
     return lon, lat, time_hr
 
@@ -87,12 +87,12 @@ def rearrange_orbit(cycle_duration: float, lon: np.ndarray, lat: np.ndarray,
     return lon, lat, time, time[indexes[0]]
 
 
-def select_box(box: math.Box, lon: np.ndarray, lat: np.ndarray,
+def select_box(box: math_func.Box, lon: np.ndarray, lat: np.ndarray,
                time: np.ndarray
                ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """Selects the orbit in the defined box."""
     mask = box.within(lon, lat)
-    x_al = math.curvilinear_distance(lon, lat, VOLUMETRIC_MEAN_RADIUS)
+    x_al = math_func.curvilinear_distance(lon, lat, VOLUMETRIC_MEAN_RADIUS)
     return lon[mask], lat[mask], time[mask], x_al[mask]
 
 
@@ -100,7 +100,7 @@ class Orbit:
     """Properties of one orbit"""
     def __init__(self, height: float, lat: np.ndarray, lon: np.ndarray,
                  pass_time: np.ndarray, time: np.ndarray, x_al: np.ndarray,
-                 shift_time: Optional[float]):
+                 al_cycle, shift_time: Optional[float]):
         self.height = height
         self.lat = lat
         self.lon = lon
@@ -108,6 +108,7 @@ class Orbit:
         self.shift_time = shift_time
         self.time = time
         self.x_al = x_al
+        self.al_cycle = al_cycle
 
     def passes_per_cycle(self):
         return len(self.pass_time)
@@ -135,7 +136,7 @@ class Pass:
     """Handle one pass"""
     def __init__(self, lat_nadir: np.ndarray, lat: np.ndarray,
                  lon_nadir: np.ndarray, lon: np.ndarray, time: np.ndarray,
-                 x_ac: np.ndarray, x_al: np.ndarray):
+                 x_ac: np.ndarray, x_al: np.ndarray, al_cycle:float):
         self.lat_nadir = lat_nadir
         self.lat = lat
         self.lon_nadir = lon_nadir
@@ -145,6 +146,7 @@ class Pass:
         self._time = None
         self.x_ac = x_ac
         self.x_al = x_al
+        self.al_cycle = al_cycle
 
     @property
     def time(self):
@@ -171,7 +173,7 @@ def calculate_orbit(parameters: settings.Parameters,
                            "'ephemeris_cols' parameter well defined?")
 
     # Put longitude in [-180, 180[
-    lon = math.normalize_longitude(lon)
+    lon = math_func.normalize_longitude(lon)
 
     for key, value in properties.items():
         if key == 'cycle_duration':
@@ -203,7 +205,7 @@ def calculate_orbit(parameters: settings.Parameters,
     del indexes
 
     if parameters.shift_lon is not None:
-        lon = math.normalize_longitude(lon + parameters.shift_lon)
+        lon = math_func.normalize_longitude(lon + parameters.shift_lon)
 
     # Rearrange orbit starting from pass 1
     lon, lat, time, pass_time = rearrange_orbit(cycle_duration, lon, lat, time)
@@ -219,9 +221,9 @@ def calculate_orbit(parameters: settings.Parameters,
     time = np.interp(x_al, distance[:-1], time[:-1])
     lon = np.interp(x_al, distance[:-1], lon[:-1])
     lat = np.interp(x_al, distance[:-1], lat[:-1])
-
+    al_cycle = distance[-1]
     return Orbit(parameters.height, lat, lon, np.sort(pass_time), time, x_al,
-                 parameters.shift_time)
+                 al_cycle, parameters.shift_time)
 
 
 def calculate_pass(pass_number: int, orbit: Orbit,
@@ -241,6 +243,7 @@ def calculate_pass(pass_number: int, orbit: Orbit,
     lat_nadir = orbit.lat[indexes]
     time = orbit.time[indexes]
     x_al = orbit.x_al[indexes]
+    al_cycle = orbit.al_cycle
 
     # Compute accross track distances from nadir
     # Number of points in half of the swath
@@ -250,10 +253,10 @@ def calculate_pass(pass_number: int, orbit: Orbit,
     x_ac = np.hstack((-np.flip(x_ac), x_ac))
 
     location = np.ascontiguousarray(
-        np.vstack(math.spher2cart(lon_nadir, lat_nadir)).T)
-    satellite_direction = math.satellite_direction(location)
-    lon, lat = math.calculate_swath(parameters.delta_ac, parameters.half_gap,
+        np.vstack(math_func.spher2cart(lon_nadir, lat_nadir)).T)
+    satellite_direction = math_func.satellite_direction(location)
+    lon, lat = math_func.calculate_swath(parameters.delta_ac, parameters.half_gap,
                                     half_swath, VOLUMETRIC_MEAN_RADIUS,
                                     location, satellite_direction)
 
-    return Pass(lat_nadir, lat, lon_nadir, lon, time, x_ac, x_al)
+    return Pass(lat_nadir, lat, lon_nadir, lon, time, x_ac, x_al, al_cycle)
