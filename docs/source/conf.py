50ae7c9a270f2617cfb085f9096ec0b54dd3ddc1
Frédéric BRIOL <fbriol@gmail.com>
2020-05-22 19:05:25 +0200

    Updating version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,9 +22,9 @@ copyright = '(2020, CNES/JPL)'
 author = 'CNES/JPL'
 
 # The short X.Y version
-version = '0.0.0'
+version = '0.1.0'
 # The full version, including alpha/beta/rc tags
-release = '0.0.0'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 
