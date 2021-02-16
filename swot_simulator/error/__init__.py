f93d6df05632988a3a5b74429dff4007a718d9c5
Frédéric BRIOL <fbriol@gmail.com>
2021-02-16 05:17:29 +0100

    revert

--- swot_simulator/error/__init__.py
+++ swot_simulator/error/__init__.py
@@ -2,3 +2,10 @@
 #
 # All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
+from .altimeter import Altimeter
+from .baseline_dilation import BaselineDilation
+from .corrected_roll_phase import CorrectedRollPhase
+from .karin import Karin
+from .roll_phase import RollPhase
+from .timing import Timing
+from .wet_troposphere import WetTroposphere
