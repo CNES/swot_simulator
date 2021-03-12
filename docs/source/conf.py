20bbe007a19cdf8c48f7ce5d5fcdacb9864e3d6d
Frédéric BRIOL <fbriol@gmail.com>
2021-03-12 18:10:52 +0100

    v1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,9 +22,9 @@ copyright = '(2021, CNES/JPL)'
 author = 'CNES/JPL'
 
 # The short X.Y version
-version = '0.2.0'
+version = '1.0.0'
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 
