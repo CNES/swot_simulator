b408884520accee8994323adebe975e8bac335ab
Frédéric BRIOL <fbriol@gmail.com>
2021-03-13 15:30:56 +0100

    missing docstring

--- swot_simulator/error/__init__.py
+++ swot_simulator/error/__init__.py
@@ -2,6 +2,10 @@
 #
 # All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
+"""
+Simulate errors
+---------------
+"""
 from .altimeter import Altimeter
 from .baseline_dilation import BaselineDilation
 from .corrected_roll_phase import CorrectedRollPhase
