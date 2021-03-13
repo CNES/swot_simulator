ad4ca3325fe760aad5308a7fbe29b46b385ff4a9
Frédéric BRIOL <fbriol@gmail.com>
2021-03-13 14:51:47 +0100

    yapf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,7 @@
 
 import os
 import sys
+
 HERE = os.path.normpath(
     os.path.join(os.path.dirname(os.path.abspath(__file__))))
 sys.path.insert(0, os.path.normpath(os.path.join(HERE, "..", "..")))
