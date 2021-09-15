d7007d4f9764f124b24b2ec00a76926f3d3ca009
Frederic Briol <fbriol@gmail.com>
2021-09-15 16:58:25 +0200

    bump to version 1.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,9 +22,9 @@ copyright = '(2021, CNES/JPL)'
 author = 'CNES/JPL'
 
 # The short X.Y version
-version = '1.2.0'
+version = '1.2.1'
 # The full version, including alpha/beta/rc tags
-release = '1.2.0'
+release = '1.2.1'
 
 # -- General configuration ---------------------------------------------------
 
